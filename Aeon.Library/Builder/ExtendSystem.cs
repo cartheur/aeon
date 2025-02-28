fecc108a8c8320a34b85e6b57924b3dced748a60
cartheur <cartheur@pm.me>
2025-02-28 14:35:47 +0100

    Amend copyright statement

--- Aeon.Library/Builder/ExtendSystem.cs
+++ Aeon.Library/Builder/ExtendSystem.cs
@@ -1,3 +1,6 @@
+//
+// Copyright 2003 - 2025, all rights reserved. No rights are explicitly granted to persons who have obtained this source code whose sole purpose is to illustrate the method of attaining AGI. Contact m.e. at: cartheur@pm.me.
+//
 using System.Xml.Linq;
 
 namespace Aeon.Library.Builder
--- Aeon.Library/Utilities/CustomTag.cs
+++ Aeon.Library/Utilities/CustomTag.cs
@@ -1,5 +1,5 @@
 ﻿//
-// This autonomous intelligent system software is the property of Cartheur Research B.V. Copyright 2003 - 2025, all rights reserved.
+// Copyright 2003 - 2025, all rights reserved. No rights are explicitly granted to persons who have obtained this source code whose sole purpose is to illustrate the method of attaining AGI. Contact m.e. at: cartheur@pm.me.
 //
 namespace Aeon.Library
 {
